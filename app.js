929e1854c0a20f75f4996f54691eb9e5038b4877
Rishi Karri <rishikarri1@gmail.com>
2017-04-23 18:15:21 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var a = 1; 
+var b = 2; 
+var c = a + b; 
+
+// logs data values to the browser console which helps when you are trying to figure out what is going with your program 
+// console.log()
+console.log('***')
+console.log(c);
+console.log('***')
+// this will output the value to the command line window 
