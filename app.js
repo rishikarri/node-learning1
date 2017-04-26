df28236983a5be3e3fbea44bf97927ab0f870873
Rishi Karri <rishikarri1@gmail.com>
2017-04-26 14:37:15 -0400

    created greet folder

--- app.js
+++ app.js
@@ -46,7 +46,66 @@
 // node grabs this file and then executed the javascript file 
 // set a variable equal to the results of the function 
 
-// set greetingssenor to whatever is returned by greet.js - in this case it is the function called greet so we can now call greetingssenor as a function
-const greetingsSenor = require('./greet.js');
+// // set greetingssenor to whatever is returned by greet.js - in this case it is the function called greet so we can now call greetingssenor as a function
+// const greetingsSenor = require('./greet.js');
 
-greetingsSenor();
+// greetingsSenor();
+
+// const person = {
+// 	firstName: 'John',
+// 	lastName: 'Doe',
+// 	greet: function() {
+// 		console.log('Hello' + this.firstName + '' + this.lastName);
+// 	}
+// };
+
+// person.greet();
+// console.log(person['firstName']);
+
+// person['pet'] = 'dog'
+
+// console.log(person['pet']);
+
+// class Person {
+// 	constructor(firstName, lastName){
+// 		this.firstName = firstName;
+// 		this.lastName = lastName;
+// 	}
+// }
+
+// const travis = new Person('travis', 'scott'); 
+
+// console.log(travis.firstName);
+
+// Person.prototype.greet = () => {
+// 	console.log('hello' + this.firstName);
+// 	return;
+// }
+
+// travis.greet();
+
+
+// pass by value
+function change(b){
+	b = 2; 
+}
+
+var a = 1; 
+change(a);
+console.log(a);
+
+// pass by reference 
+
+function changeObj(d) {
+	d.prop1 = function() {}
+	d.prop2 = {}
+}
+
+var c = {}
+c.prop1 = {}; 
+
+changeObj(c);
+console.log(c);
+
+// when we call change object, the d value points to the same variable that c points to and makes sure that the original object changes because of pass by reference 
+
