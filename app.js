05a0b7f7d0845e3c905d0b0d862792d8f2490316
Rishi Karri <rishikarri1@gmail.com>
2017-04-27 16:06:02 -0400

    learned quirk of exports shorthand

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-// var greet = require('./greet1');
-// var greet2 = require('./greet2.js').greet;
-// var greet3 = require('./greet3.js');
+const greet = require('./greet.js');
 
-// // because I replaced exports with a function, what we'll do is call that function because the variable is pointing at it 
-
-
-
-// // var greet4 = require('./greet4');
-// // var grtr = new greet4(); 
-// // grtr.greet();
-
-
-// var greet5 = require('./greet5.js');
-
-// var grtr = new greet5(); 
-// grtr.greet();
-
-
-const greet6 = require('./greet6.js');
-
-var greeter = new greet6('english');
-
-greeter.greet();
+greet.greet();
--- greet.js
+++ greet.js
@@ -1,10 +1,6 @@
-// the code inside greet.js is protected - app.js cannot get go get functions within greet js
-
-var greet = () => {
-	console.log('hi');
+exports.greet = () => {
+	console.log('Hello');
 }
 
-// a place to put things that I want to make available to anything else that uses this module
-// this module is exposing the greet function
-
-module.exports = greet; 
+// console.log(exports);
+// console.log(module.exports);
