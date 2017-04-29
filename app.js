70c741d4f91078fb09b4ce4911ae49681283297d
Rishi Karri <rishikarri1@gmail.com>
2017-04-29 17:23:48 -0400

    learning how to use built-in emitter

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const emitter = require('./emitter2.js');
+// grabbing emitter - you simply just need to grab them from events 
+var Emitter = require('events');
 
-emtr = new emitter();
-// now has an empty events object
+var emtr = new Emitter();
 
-emtr.on('greet', function() {
-	console.log('it is lit');
-});
+emtr.on('greet', function(){
+	console.log('somewhere someoene said hello');
+})
+
+emtr.on('greet', function(){
+	console.log(' it is lit')
+})
 
 emtr.emit('greet');
