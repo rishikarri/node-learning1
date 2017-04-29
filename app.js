2b4d39d75fd6e3a118ad316bc031a949b7d0dcbe
Rishi Karri <rishikarri1@gmail.com>
2017-04-29 17:39:18 -0400

    learned how to create prototype directly and then create objects using that prototype using Object.create

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-// grabbing emitter - you simply just need to grab them from events 
-var Emitter = require('events');
+// // magic string - a string that has some special meaning in our code - this is bad because we can't find errors as easily - let's make them variables 
+// const Emitter = require('events');
+// const eventConfig = require('./config.js').events;
 
-var emtr = new Emitter();
+// // grabbing emitter - you simply just need to grab them from events 
 
-emtr.on('greet', function(){
-	console.log('somewhere someoene said hello');
-})
 
-emtr.on('greet', function(){
-	console.log(' it is lit')
-})
+// const emtr = new Emitter();
 
-emtr.emit('greet');
+// emtr.on(eventConfig.GREET, () => {
+// 	console.log('somewhere someoene said hello');
+// })
+
+// emtr.on(eventConfig.GREET, () => {
+// 	console.log(' it is lit')
+// })
+
+// emtr.emit('greet');
+
+var person = {
+	firstname: '',
+	lastname: '', 
+	greet: function() {
+		return this.firstname; 
+	}
+}
+
+// I want this to be the prototype of some other objects that I construct - i am just building the object that I want to be the prototype
+
+// when i'm done I will have an empty object who's prototype is person
+var john = Object.create(person);
+
+john.lastname = 'doe';
+john.firstname = 'john';
+
+console.log(john.greet());
