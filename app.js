cddfe7d045d829c70c93f51f07ebaf94c166f20f
Rishi Karri <rishikarri1@gmail.com>
2017-05-02 13:29:29 -0400

    successfully read file and saved it to variable greet

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ var fs = require('fs');
 // I can read the text file now - we are dealing with files on our operating system 
 
 // takes a few parameters, first thing it takes is the location of the file 
-var greet = fs.readFileSync()
+// __dirname gives me the path to the directory that the code i'm writing lives in 
+
+// looking at the file, will read the binary data and then will use the encoding to determine what the 0s and 1s mean
+var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8')
+console.log(greet)
+// var greet = fs.readFileSync('./greet.txt')
