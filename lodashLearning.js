626e8e8de325d2b0a5a5d2dae9a077e8e2db5f47
Rishi Karri <rishikarri1@gmail.com>
2017-05-26 10:10:01 -0400

    lodash learning

---
+++ lodashLearning.js
@@ -0,0 +1,11 @@
+//let's learn some lodash 
+
+const people = [
+	{name: 'Frank', age: 30},
+	{name: 'Susan', age: 10},
+	{name: 'Bill', age: 22},
+	{name: 'Carol', age: 52},
+	{name: 'Stephanie', age: 22},
+	{name: 'Kyle', age: 68},
+	{name: 'Hank', age: 22},
+]
