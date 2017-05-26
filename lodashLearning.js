0a42194c31fc7de34a6dcb0e9878414a2eb3a486
Rishi Karri <rishikarri1@gmail.com>
2017-05-26 10:22:44 -0400

    succesfully getting lodash to run simple functions

--- lodashLearning.js
+++ lodashLearning.js
@@ -1,6 +1,7 @@
 //let's learn some lodash 
+var _ = require('lodash');
 
-const people = [
+var people = [
 	{name: 'Frank', age: 30},
 	{name: 'Susan', age: 10},
 	{name: 'Bill', age: 22},
@@ -9,3 +10,9 @@ const people = [
 	{name: 'Kyle', age: 68},
 	{name: 'Hank', age: 22},
 ]
+
+// lodash a helpful set of utilities to quickly iterate and create iterables
+
+var result = _(people).sortBy('age').value();
+
+console.log(result);
