23d662814fb1ede21f73f8f8df1692be5d2a7500
Rishi Karri <rishikarri1@gmail.com>
2017-06-16 11:04:33 -0400

    tested a node function

--- greeter.js
+++ greeter.js
@@ -1,16 +1,26 @@
-var EventEmitter = require('events');
+// var EventEmitter = require('events');
 
-module.exports = class Greeter extends EventEmitter {
-	constructor() {
-		super();
-		this.greeting = 'hello world!';
-	}
+// module.exports = class Greeter extends EventEmitter {
+// 	constructor() {
+// 		super();
+// 		this.greeting = 'hello world!';
+// 	}
 
-	greet(data) {
-		console.log(this.greeting)
-		this.emit('greet', data)
-	}
-	sayHello() {
-		console.log('hello');
-	}
+// 	greet(data) {
+// 		console.log(this.greeting)
+// 		this.emit('greet', data)
+// 	}
+// 	sayHello() {
+// 		console.log('hello');
+// 	}
+// }
+
+// function addition(a, b) {
+// 	return a + b; 
+// }
+
+const addition = (a,b) => {
+	return a + b; 
 }
+
+console.log(addition(5,5));
