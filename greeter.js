6714b7d5a2c265b80484b7df39c151858dedb452
Rishi Karri <rishikarri1@gmail.com>
2017-09-25 13:52:26 -0400

    node learning

--- greeter.js
+++ greeter.js
@@ -22,3 +22,5 @@ const b = [1, 21];
 // learned that you need babel to be able to compile the new spread operator 
 //also learned how to use redux logic 
 // 75% done through udemy course
+// node essentially takes the v8 engine out of javascript and allows you to run it on a machine
+// basically you are able to use javascript to run serverside technology when previously it could only be run in a browser
