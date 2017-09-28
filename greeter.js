50a3e8791b2384e3287e88ede12b4c1345553d12
Rishi Karri <rishikarri1@gmail.com>
2017-09-28 12:02:18 -0400

    understanding global npm installation

--- greeter.js
+++ greeter.js
@@ -23,6 +23,10 @@ const b = [1, 21];
 // jasmine-node is a tool that helps us write automated tests - automatically teest my software as i build it 
 // the above is a dev dependency - i don't need it for the app to run but i do need it as a developer 
 
+// npm install -g nodemon - globally install this module - don't download this module into the nodemodules folder for this particular app
+// but rather put it in a location so that npm can access it from any point in my computer 
+
+
 // --save-dev - this is now a development dependency
 // learned that you need babel to be able to compile the new spread operator 
 //also learned how to use redux logic 
