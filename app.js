bceb504b8db927aa4a3b6a16202bce37b5a25b72
Rishi Karri <rishikarri1@gmail.com>
2017-10-28 16:58:03 -0400

    load public static files using app.use in express

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const app = express(); // this is a working express app
 // use environment variables to make sure we are listening on the right port
 
 const port = process.env.PORT || 3000; 
+
+app.use ('/assets', express.static(__dirname + '/public'))
+
+//everytime we see /assets, what i'll do is go find this full file path public and return these files 
 // this variable port will be either equal to the environment variable or local host 3000  
 
 // if we have a get request at the home page, this function will fire 
 app.get('/', function(req, res) {
 	console.log(req, 'req');
-	res.send('<html><head></head> <body><h1>hello w </h1></body></html>')
+	res.send('<html><head> <link href=assets/styles.css type=text/css rel=stylesheet /> </head> <body><h1>hello w </h1></body></html>')
 })
+
+// the browswer loaded and then there was  link to assets / styles.css - which was available in public and then it loaded styles.css
 
 app.get('/api', (req, res) => {
 	res.json({
